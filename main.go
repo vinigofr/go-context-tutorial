5c8808fcd02297469ad7dbf4468812f13676a925
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-27 00:05:43 -0300

    Adiciona demonstração de contexto

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func enrichContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, "api-key", "my-super-secret-api-key")
+}
+
+func doSomethingCool(ctx context.Context) {
+	apiKey := ctx.Value("api-key")
+	fmt.Println(apiKey)
+}
+
+func main() {
+	fmt.Println("Go context Tutorial")
+
+	ctx := context.Background()
+	ctx = enrichContext(ctx)
+
+	doSomethingCool(ctx)
+}
